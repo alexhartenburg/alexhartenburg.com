360b20327107f47cf804b0218bb71de4dd7c58e2
Alex Hartenburg <alexhartenburg@gmail.com>
2024-01-19 06:53:14 -0800

    Fix top and bottom row sizing bug. Refactor background to take size and density inputs and programatically assemble the tessalation. Still need to fine tune spacing and sizing, because at high densities, the honeycomb gets smaller than the glow.

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,40 +1,52 @@
-import { BlurContainer, Glow, SilhouetteContainer, SilhouetteRow, Silhouette } from "./Background_styled"
+import { BlurContainer, Glow, SilhouetteContainer, SilhouetteRowFirst, SilhouetteRow, SilhouetteRowLast, Silhouette } from "./Background_styled"
 
 const Background = () => {
+    const size = 100;
+    const density = 5;
+    const returnSilhouettes = (x:number) => {
+        let arr = [];
+        for(let i=0; i<x; i++){
+            arr.push(<Silhouette size={size} density={density} />)
+        };
+        return arr;
+    }
+    const returnAscendingRows = () => {
+        let arr = [];
+        for(let i=0; i<density; i++){
+            arr.push(<SilhouetteRow size={size} density={density}>{returnSilhouettes(3+i+density)}</SilhouetteRow>)
+        }
+        return arr;
+    }
+    const returnStraightRows = () => {
+        let arr = [];
+        let localDensity = density + (density%2 - 1);
+        for(let i=0; i<2+localDensity; i++){
+            arr.push(<SilhouetteRow size={size} density={density}>{returnSilhouettes(3+2*density-i%2)}</SilhouetteRow>)
+        }
+        return arr;
+    }
+    const returnDescendingRows = () => {
+        let arr = [];
+        for(let i=0; i<density; i++){
+            arr.push(<SilhouetteRow size={size} density={density}>{returnSilhouettes(2+2*density-i)}</SilhouetteRow>)
+        }
+        return arr;
+    }
     return(
         <>
         <BlurContainer>
-            <Glow />
+            <Glow size={size}/>
         </BlurContainer>
-        <SilhouetteContainer>
-            <SilhouetteRow>
-                <Silhouette />
-            </SilhouetteRow>
-            <SilhouetteRow>
-                <Silhouette />
-                <Silhouette />
-            </SilhouetteRow>
-            <SilhouetteRow>
-                <Silhouette />
-                <Silhouette />
-                <Silhouette />
-            </SilhouetteRow>
-            <SilhouetteRow>
-                <Silhouette />
-                <Silhouette />
-            </SilhouetteRow>
-            <SilhouetteRow>
-                <Silhouette />
-                <Silhouette />
-                <Silhouette />
-            </SilhouetteRow>
-            <SilhouetteRow>
-                <Silhouette />
-                <Silhouette />
-            </SilhouetteRow>
-            <SilhouetteRow>
-                <Silhouette />
-            </SilhouetteRow>
+        <SilhouetteContainer size={size}>
+            <SilhouetteRowFirst size={size} density={density}>
+                {returnSilhouettes(2+density)}
+            </SilhouetteRowFirst>
+            {returnAscendingRows()}
+            {returnStraightRows()}
+            {returnDescendingRows()}
+            <SilhouetteRowLast size={size} density={density}>
+                {returnSilhouettes(2+density)}
+            </SilhouetteRowLast>
         </SilhouetteContainer>
         </>
     )
--- src/components/Background/Background_styled.tsx
+++ src/components/Background/Background_styled.tsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 import { keyframes } from "styled-components";
 
+const h = 60;
+const n = 7;
+
+
+
+
 const BlurContainer = styled.div`
     width: 100vw;
     height: 100vh;
@@ -23,44 +29,63 @@ const glowAnimation = keyframes`
         background-position: right top;
     }
 `
-const Glow = styled.div`
+const Glow = styled.div<{ size: number }>`
     position: absolute;
-    width: min(60vw, 60vh);
-    height: min(60vw, 60vh);
+    width: min(${(props) => props.size || 50}vw, ${(props) => props.size || 50}vh);
+    height: min(${(props) => props.size || 50}vw, ${(props) => props.size || 50}vh);
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    clip-path: polygon(0% 25%, 0% 75%, 50% 100%, 100% 75%, 100% 25%, 50% 0%);
+    clip-path: polygon(0% 30%, 0% 70%, 25% 100%, 75% 100%, 100% 70%, 100% 30%, 75% 0%, 25% 0%);
     background-image: linear-gradient(45deg, #444 45%, #555 50%, #444 55%);
     background-size: 220% 220%;
     animation: ${glowAnimation} 10s linear infinite;
 `
-const SilhouetteContainer = styled.div`
+const SilhouetteContainer = styled.div<{ size: number }>`
     position: absolute;
-    width: min(60vw, 60vh);
-    height: min(60vw, 60vh);
+    width: min(${(props) => props.size || 50}vw, ${(props) => props.size || 50}vh);
+    height: min(${(props) => props.size || 50}vw, ${(props) => props.size || 50}vh);
     top: 50%;
     left: 50%;
     padding: 0;
     margin: 0;
+    overflow: visible;
     transform: translate(-50%, -50%);
-    clip-path: polygon(0% 25%, 0% 75%, 50% 100%, 100% 75%, 100% 25%, 50% 0%);
 `
-const SilhouetteRow = styled.div`
+const SilhouetteRow = styled.div<{ size: number, density: number }>`
     display: block;
     text-align: center;
     width: 100%;
+    height: calc(min(${(props) => props.size || 50}vw, ${(props) => props.size || 50}vh) / ${(props) => 4 + 3 * props.density || 6});
     padding: 0;
-    margin: 0;
+    margin: calc(calc(min(${(props) => props.size || 50}vw, ${(props) => props.size || 50}vh) / ${(props) => 4 + 3 * props.density || 6}) / -5) 0;
     height: calc(min(10, 10) - 12px);
 `
-const Silhouette = styled.div`
+const SilhouetteRowFirst = styled.div<{ size: number, density: number }>`
+    display: block;
+    text-align: center;
+    width: 100%;
+    height: calc(min(${(props) => props.size || 50}vw, ${(props) => props.size || 50}vh) / ${(props) => 4 + 3 * props.density || 6});
+    padding: 0;
+    margin: calc(calc(min(${(props) => props.size || 50}vw, ${(props) => props.size || 50}vh) / ${(props) => 4 + 3 * props.density || 6}) / 10) 0 calc(calc(min(${(props) => props.size || 50}vw, ${(props) => props.size || 50}vh) / ${(props) => 4 + 3 * props.density || 6}) / -5) 0;
+    height: calc(min(10, 10) - 12px);
+`
+const SilhouetteRowLast = styled.div<{ size: number, density: number }>`
+    display: block;
+    text-align: center;
+    width: 100%;
+    height: calc(min(${(props) => props.size || 50}vw, ${(props) => props.size || 50}vh) / ${(props) => 4 + 3 * props.density || 6});
+    padding: 0;
+    margin: calc(calc(min(${(props) => props.size || 50}vw, ${(props) => props.size || 50}vh) / ${(props) => 4 + 3 * props.density || 6}) / -5) 0;
+    height: calc(min(10, 10) - 12px);
+`
+const Silhouette = styled.div<{ size: number, density: number }>`
     display: inline-block;
-    width: calc(min(10vw, 10vh) - 1px);
-    height: calc(min(10vw, 10vh) - 1px);
-    margin: -6px 2px;
+    width: calc(calc(min(${(props) => props.size || 50}vw, ${(props) => props.size || 50}vh) / ${(props) => 4 + 3 * props.density || 6}) - 1px);
+    height: calc(calc(min(${(props) => props.size || 50}vw, ${(props) => props.size || 50}vh) / ${(props) => 4 + 3 * props.density || 6}) - 1px);
+    margin: 0 calc(min(${(props) => props.size || 50}vw, ${(props) => props.size || 50}vh) / 100);
     clip-path: polygon(0% 25%, 0% 75%, 50% 100%, 100% 75%, 100% 25%, 50% 0%);
     background-color: #222;
 `
 
-export { BlurContainer, Glow, SilhouetteContainer, SilhouetteRow, Silhouette }
+export { BlurContainer, Glow, SilhouetteContainer, SilhouetteRowFirst, SilhouetteRow, SilhouetteRowLast, Silhouette }
