df5da562135c20b5b56596817c37e264a029e4da
Alex Hartenburg <alexhartenburg@gmail.com>
2024-01-28 17:19:06 -0800

    Add opacity mask and pull the animation speen for opacity, position, and color out as variables to tune them easier. Tuned the animation and tuned the gradient of the content pane.

--- src/components/ContentPane/ContentPane.tsx
+++ src/components/ContentPane/ContentPane.tsx
@@ -9,7 +9,7 @@ const Pane = styled.div`
   height: 50vh;
   border-radius: 10px;
   border: 2px solid rgba(45, 45, 45, 0.8);
-  background-image: linear-gradient(45deg, rgba(45, 45, 45, 0.95), rgba(60, 60, 60, 0.95));
+  background-image: linear-gradient(45deg, rgba(35, 35, 35, 0.93), rgba(60, 60, 60, 0.93));
   box-shadow: -10px 5px 10px 5px rgba(34,34,34, 0.95);
 `;
 
